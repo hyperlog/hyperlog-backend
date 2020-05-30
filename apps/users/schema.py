5347a227795654bb3b3cde8e04211f05936d7f99
nikochiko <kaustubh299792458@gmail.com>
2020-05-31 01:00:50 +0530

    Add delete and update mutations for users/profiles

--- apps/users/schema.py
+++ apps/users/schema.py
@@ -1,11 +1,16 @@
+import logging
+
 import graphene
 import graphql_jwt
 from graphene_django import DjangoObjectType
 from graphql_jwt.decorators import login_required
 
-from django.contrib.auth import logout
+from django.contrib.auth import get_user_model, logout
+from django.db.utils import Error as DjangoDBError
 
 from apps.users.models import User
+
+logger = logging.getLogger(__name__)
 
 
 class UserType(DjangoObjectType):
@@ -85,9 +90,82 @@ class Logout(graphene.Mutation):
         return Logout(success=True)
 
 
+class UpdateUser(graphene.Mutation):
+    """Mutation to update user's profile details"""
+
+    success = graphene.Boolean()
+    errors = graphene.List(graphene.String)
+
+    class Arguments:
+        username = graphene.String()
+        password = graphene.String()
+        email = graphene.String()
+        first_name = graphene.String()
+        last_name = graphene.String()
+
+    @login_required
+    def mutate(self, info, **kwargs):
+        user = info.context.user
+
+        for unique_field in ["username", "email"]:
+            # Check if arg is given and if it is different from existing
+            if kwargs.get(unique_field) and getattr(
+                user, unique_field
+            ) != kwargs.get(unique_field):
+                val = kwargs[unique_field]
+
+                # Check for unique constraint
+                if (
+                    get_user_model()
+                    .objects.filter(**{unique_field: val})
+                    .exists()
+                ):
+                    errors = [f"User with {unique_field} {val} already exists"]
+                    return UpdateUser(success=False, errors=errors)
+
+                setattr(user, unique_field, val)
+
+        for field in ["first_name", "last_name"]:
+            if kwargs.get(field):
+                setattr(user, field, kwargs.get(field))
+
+        if kwargs.get("password"):
+            user.set_password(kwargs.get("password"))
+
+        try:
+            user.save()
+            return UpdateUser(success=True)
+        except DjangoDBError as err:
+            return UpdateUser(success=False, errors=[str(err)])
+        except Exception:
+            # Hide the error log from user here as it could be a bug
+            logger.error("Error in UpdateUser mutation", exc_info=True)
+            return UpdateUser(success=False, errors=["server error"])
+
+
+class DeleteUser(graphene.Mutation):
+    """Mutation to delete a user"""
+
+    success = graphene.Boolean()
+    errors = graphene.List(graphene.String)
+
+    @login_required
+    def mutate(self, info, **kwargs):
+        user = info.context.user
+
+        try:
+            user.delete()
+            return DeleteUser(success=True)
+        except Exception:
+            logger.error("Error in DeleteUser mutation")
+            return DeleteUser(success=False, errors=["server error"])
+
+
 class Mutation(object):
     login = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
     register = Register.Field()
     logout = Logout.Field()
+    update_user = UpdateUser.Field()
+    delete_user = DeleteUser.Field()
