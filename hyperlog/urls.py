31a30887a42ead216ff55b3fba51bdd181918166
nikochiko <kaustubh299792458@gmail.com>
2020-06-09 21:02:46 +0530

    Setup JWT-cookie

--- hyperlog/urls.py
+++ hyperlog/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
+from graphql_jwt.decorators import jwt_cookie
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("graphql/", jwt_cookie(GraphQLView.as_view(graphiql=True))),
     path("", include("apps.profiles.urls", namespace="profiles")),
 ]
