750a14ba3e9e271256e6b79fffb01fd5acc34d29
nikochiko <kaustubh299792458@gmail.com>
2020-06-09 19:01:39 +0530

    Add UnicodeUsernameValidator to username validators

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils import timezone
 
@@ -50,10 +51,17 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(
-        verbose_name="Username", unique=True, max_length=30
+        verbose_name="Username",
+        unique=True,
+        max_length=30,
+        validators=[UnicodeUsernameValidator()],
+        error_messages={"unique": "A user with that username already exists"},
     )
     email = models.EmailField(
-        verbose_name="Email", unique=True, max_length=255
+        verbose_name="Email",
+        unique=True,
+        max_length=255,
+        error_messages={"unique": "A user with that email id already exists"},
     )
     first_name = models.CharField(
         verbose_name="First name", max_length=30, default="first"
