bd21fec69fc6f8b8ba513eb86c35a41e5056e4e2
nikochiko <kaustubh299792458@gmail.com>
2020-06-21 19:52:54 +0530

    Create all stripe models

--- apps/membership/models.py
+++ apps/membership/models.py
@@ -1,6 +1,7 @@
 import json
 
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.core.validators import MinLengthValidator
 
 from apps.base.utils import validate_lowercase
@@ -12,7 +13,7 @@ class StripeProduct(models.Model):
 
     # For docs on ID length see:
     # https://stripe.com/docs/upgrades#what-changes-does-stripe-consider-to-be-backwards-compatible  # noqa
-    id = models.CharField(max_length=255)
+    stripe_id = models.CharField(max_length=255)
     active = models.BooleanField()
     # For docs on max length of name and description, see:
     # https://stripe.com/docs/upgrades#2018-10-31
@@ -46,7 +47,7 @@ class StripePrice(models.Model):
 
     # For docs on ID length see:
     # https://stripe.com/docs/upgrades#what-changes-does-stripe-consider-to-be-backwards-compatible  # noqa
-    id = models.CharField(max_length=255)
+    stripe_id = models.CharField(max_length=255)
     active = models.BooleanField(default=True)
     # Should be exactly 3 characters, all lowercase and supported by stripe
     currency = models.CharField(
@@ -114,3 +115,56 @@ class StripePrice(models.Model):
         return locals()
 
     recurring = property(**recurring())
+
+
+class StripeCustomer(models.Model):
+    """Ref: https://stripe.com/docs/api/customers"""
+
+    # For docs on ID length see:
+    # https://stripe.com/docs/upgrades#what-changes-does-stripe-consider-to-be-backwards-compatible  # noqa
+    stripe_id = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    email = models.EmailField(max_length=255)
+    _metadata_json = models.TextField(
+        default=json.dumps({})
+    )  # JSON-encoded key-value pairs
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="Stripe Customer",
+    )
+
+    def metadata():
+        doc = "The metadata attribute of Product object. key-value pairs."
+
+        def fget(self):
+            return json.loads(self._metadata_json)
+
+        def fset(self, value):
+            self._metadata_json = json.dumps(value)
+            self.save()
+
+        def fdel(self):
+            self._metadata_json = json.dumps({})
+            self.save()
+
+        return locals()
+
+    metadata = property(**metadata())
+
+
+class StripeSubscription(models.Model):
+    """Ref: https://stripe.com/docs/billing/subscriptions/fixed-price#create-subscription"""  # noqa
+
+    # For docs on ID length see:
+    # https://stripe.com/docs/upgrades#what-changes-does-stripe-consider-to-be-backwards-compatible  # noqa
+    stripe_id = models.CharField(max_length=255)
+    current_period_start = models.DateTimeField()
+    current_period_end = models.DateTimeField()
+    customer = models.ForeignKey(
+        StripeCustomer, on_delete=models.CASCADE, related_name="subscriptions"
+    )
+    # Disallows deleting price if a subscription using that price exists
+    price = models.ForeignKey(
+        StripePrice, on_delete=models.PROTECT, related_name="subscriptions"
+    )
