47e88e7bf1ea1fb5b38dca5cc104928de2859fc1
nikochiko <kaustubh299792458@gmail.com>
2020-07-11 22:17:10 +0530

    Add sentry integration

--- hyperlog/settings.py
+++ hyperlog/settings.py
@@ -13,6 +13,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    "https://70c4499546b84ccdb5954017d91bde23@o310860.ingest.sentry.io/1777522",  # noqa: E501
+    integrations=[DjangoIntegration()],
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
