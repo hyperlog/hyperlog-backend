3d9d3560b16e41fc895a9885c61da0df55b0cb4a
nikochiko <kaustubh299792458@gmail.com>
2020-07-13 22:38:48 +0530

    Fix daphne script

--- prod-deploy.sh
+++ prod-deploy.sh
@@ -2,4 +2,4 @@ export DEBUG=False
 
 python manage.py migrate
 python manage.py collectstatic --no-input
-daphne hyperlog.asgi:application
+daphne hyperlog.asgi:application --bind localhost --port 8000
