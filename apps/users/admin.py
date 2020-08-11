99cec9ad5b230df077a77b79d2e9678681837cf3
nikochiko <kaustubh299792458@gmail.com>
2020-08-11 18:17:28 +0530

    Regsiter GithubAuthUser model on Admin site

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from apps.users.models import User, DeletedUser
+from apps.users.models import User, DeletedUser, GithubAuthUser
 from apps.users.forms import UserChangeForm, UserCreationForm
 
 
@@ -60,3 +60,4 @@ admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
 
 admin.site.register(DeletedUser)
+admin.site.register(GithubAuthUser)
